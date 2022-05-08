6e0f6dfcf6284ef2975594c6a38acc0a847c25f9
ashleythedeveloper <ashley@harwoodslawncare.com.au>
2022-05-09 05:30:45 +1000

    fix: remove build and add licence

--- .build/src/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const signup_1 = require("./signup");
-module.exports = {
-    signupHandler: signup_1.handleSignup,
-};
-//# sourceMappingURL=index.js.map
